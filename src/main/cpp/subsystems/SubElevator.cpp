e6aabe74ad7b185edc35761411cc03578569d554
camhammond <chammond@myfairpoint.net>
2019-02-02 12:21:35 -0500

    WIP Elevator

--- src/main/cpp/subsystems/SubElevator.cpp
+++ src/main/cpp/subsystems/SubElevator.cpp
@@ -14,7 +14,38 @@ void SubElevator::InitDefaultCommand() {
 	// SetDefaultCommand(new MySpecialCommand());
 }
 
+void SubElevator::ResetPostion() {
+    // Reset the postion when bottom limit switch is made
+    elevatorDrive->SetSelectedSensorPosition(ELEVATOR_RESET_POSITION,0,0);
 
+}
+
+void SubElevator::RaiseElevatorToPosition(double position) {
+    // Raise elevator to requested postion
+    elevatorDrive->Set(ctre::phoenix::motorcontrol::ControlMode::MotionMagic, position);
+}
+
+void SubElevator::SetPositionSoftLimits(double top, double bottom) {
+    // Set the talon soft limits
+    // Maybe throw this in Configure fuction
+}
+
+void SubElevator::ConfigureHardLimits() {
+    // Configure the hard limit switches of the talon
+    // Maybe throw this in Configure fuction
+}
+
+void SubElevator::Configure() {
+    // Configure the setting for the PID etc.
+    elevatorDrive->Config_kF(0,ELEVATOR_KF_0,0);
+    elevatorDrive->Config_kP(0,ELEVATOR_KP_0,0);
+    elevatorDrive->Config_kI(0,ELEVATOR_KI_0,0);
+    elevatorDrive->Config_kD(0,ELEVATOR_KD_0,0);
+ 
+    elevatorDrive->ConfigMotionCruiseVelocity(ELEVATOR_CRUISE_VELOCITY,0);
+    elevatorDrive->ConfigMotionAcceleration(ELEVATOR_ACCELERATION,0);
+
+}
 
 // Put methods for controlling this subsystem
 // here. Call these from Commands.

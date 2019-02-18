6db9562b81ac218c446d5e6a609164352dfe2403
Lucien Hammond <legoman@myfairpoint.net>
2019-02-18 15:17:51 -0500

    Cargo and Hatch Control WIP

--- src/main/cpp/commands/CmdGrpCargoRetract.cpp
+++ src/main/cpp/commands/CmdGrpCargoRetract.cpp
@@ -7,7 +7,7 @@
 
 CmdGrpCargoRetract::CmdGrpCargoRetract(){
 	CommandGroup::AddSequential(new CmdCargoTilt(true));
-	CommandGroup::AddParallel(new CmdCargoExtend(0)); // Needs to change (0)
-	//CommandGroup::AddSequential(new CmdCargoTilt(false));
+	CommandGroup::AddParallel(new CmdCargoExtend(0)); 
+	CommandGroup::AddSequential(new CmdCargoTilt(false));
 }
 
